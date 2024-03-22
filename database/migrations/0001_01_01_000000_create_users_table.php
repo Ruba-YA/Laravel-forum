80b6b43252750c97d99b1d292b70cb06a6ac18d6
Ruba <91896958+RubaUser@users.noreply.github.com>
2024-03-23 00:50:43 +0300

    user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->boolean('is_admin')->default(0);
+            $table->boolean('is_deleted')->default(0);
+            $table->boolean('is_banned')->default(0);
+            $table->integer('rank')->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
